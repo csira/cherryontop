d5a61069631fe1a3ee36ce910939850b0cad56fa
Christopher Sira <cbsira@gmail.com>
2014-11-10 23:41:38 -0500

    [tutorial] first few tutorials to get the ball rolling

---
+++ tutorial/tutorial02.py
@@ -0,0 +1,60 @@
+"""
+2 - Additional routes
+
+Any number of routes may be bound by defining additional handlers
+wrapped with the `route`, `get`, `post`, `put`, or `delete`
+decorators.
+
+`route` is a special case only in that it provides support for
+multiple methods.
+
+It is worth mentioning here that it is completely safe to wrap the
+same handler with multiple route decorators, e.g. `post_hello_world`
+below.
+
+Sample app 2 supports the following routes:
+    GET     /
+    POST    /
+    POST    /other_post
+    PUT     /
+    DELETE  /
+    GET     /other_get
+
+"""
+
+import os
+
+import cherryontop
+import cherrypy
+
+
+class HelloWorldController(cherryontop.Controller):
+
+    @staticmethod
+    def _render(handler_name):
+        return {
+            'message': 'Hello world!',
+            'method': cherrypy.request.method,
+            'handler': handler_name,
+        }
+
+    @cherryontop.get('/')
+    def get_hello_world(self):
+        return self._render('get_hello_world')
+
+    @cherryontop.post('/')
+    @cherryontop.post('/other_post')
+    def post_hello_world(self):
+        return self._render('post_hello_world')
+
+    @cherryontop.route('/', methods=['PUT', 'DELETE'])
+    @cherryontop.get('/other_get')
+    def other_hello_world(self):
+        return self._render('other_hello_world')
+
+
+if __name__ == '__main__':
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    conf = '%s/tutorial.conf' % file_dir
+
+    cherryontop.start_server(conf, log_to_screen=True)
---
+++ tutorial/tutorial03.py
@@ -0,0 +1,65 @@
+"""
+3 - Exceptions
+
+CherryOnTop exception classes plug into the cherrypy engine to
+return JSON error descriptions to the client. That is, when a
+subclass of `CherryOnTop.CherryOnTopError` is raised e.g.
+
+    raise cherryontop.errors.HTTPError('something bad happened')
+
+and not explicitly caught, the following cascade kicks in:
+
+  - the HTTP response code is set to <exception>.http_response_code
+  - the HTTP response type is set to 'application/json'
+  - a JSON payload is rendered and returned, e.g.
+
+    {
+        'error': 'HTTPError',
+        'http_response_code': 500,
+        'message': 'something bad happened',
+    }
+
+$ curl "0.0.0.0:8080/broken" -v
+
+* About to connect() to 0.0.0.0 port 8080 (#0)
+*   Trying 0.0.0.0...
+* Adding handle: conn: 0x7fc573804000
+* Adding handle: send: 0
+* Adding handle: recv: 0
+* Curl_addHandleToPipeline: length: 1
+* - Conn 0 (0x7fc573804000) send_pipe: 1, recv_pipe: 0
+* Connected to 0.0.0.0 (0.0.0.0) port 8080 (#0)
+> GET /broken HTTP/1.1
+> User-Agent: curl/7.30.0
+> Host: 0.0.0.0:8080
+> Accept: */*
+>
+< HTTP/1.1 500 Internal Server Error
+< Date: Sun, 09 Nov 2014 14:48:27 GMT
+< Content-Length: 81
+< Content-Type: application/json
+* Server CherryPy/3.6.0 is not blacklisted
+< Server: CherryPy/3.6.0
+<
+* Connection #0 to host 0.0.0.0 left intact
+{"message":"this handler is broken","http_response_code":500,"error":"HTTPError"}
+
+"""
+
+import os
+
+import cherryontop
+
+
+class HelloWorldController(cherryontop.Controller):
+
+    @cherryontop.get('/broken')
+    def im_broken(self):
+        raise cherryontop.errors.HTTPError('this handler is broken')
+
+
+if __name__ == '__main__':
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    conf = '%s/tutorial.conf' % file_dir
+
+    cherryontop.start_server(conf, log_to_screen=True)
