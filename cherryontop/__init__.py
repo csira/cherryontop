ccc4569786c8f91b76d9f299b1ad8427081b8c6f
Christopher Sira <cbsira@gmail.com>
2014-11-27 10:23:10 -0500

    add defaults to start_server so config file optional, create strict_start_server

--- cherryontop/__init__.py
+++ cherryontop/__init__.py
@@ -2,10 +2,11 @@ from cherryontop.cache import map_all_routes
 from cherryontop.controller import Controller
 from cherryontop.decorators import delete, get, post, put, route
 from cherryontop.decorators import typecast_query_params, validate_body
-from cherryontop.spinup import start_server
+from cherryontop.spinup import start_server, strict_start_server
 
 
 __all__ = [
     'Controller', 'delete', 'get', 'map_all_routes', 'post', 'put', 'route',
-    'start_server', 'typecast_query_params', 'validate_body',
+    'start_server', 'strict_start_server',
+    'typecast_query_params', 'validate_body',
 ]
--- cherryontop/spinup.py
+++ cherryontop/spinup.py
@@ -9,18 +9,6 @@ def _create_dispatcher():
     return dispatcher
 
 
-def _setup_cp_config(path_to_cherrypy_config,
-                     path_to_access_log=None,
-                     path_to_error_log=None,
-                     log_to_screen=False):
-    if path_to_access_log:
-        cherrypy.config.update({'log.access_file': path_to_access_log})
-    if path_to_error_log:
-        cherrypy.config.update({'log.error_file': path_to_error_log})
-    cherrypy.config.update({'log.screen': log_to_screen})
-    cherrypy.config.update(path_to_cherrypy_config)
-
-
 def _daemonize():
     daemonizer = cherrypy.process.plugins.Daemonizer(cherrypy.engine)
     daemonizer.subscribe()
@@ -28,10 +16,54 @@ def _daemonize():
     sig_handler.subscribe()
 
 
-def start_server(path_to_conf, root='/', daemonize=False, **kwargs):
-    _setup_cp_config(path_to_conf, **kwargs)
+def _set_default_headers():
+    headers = cherrypy.config.get('tools.response_headers.headers', [])
+    headers.append(('Content-Type', 'application/json'))
+
+    cherrypy.config.update({
+        'tools.response_headers.headers': headers,
+        'tools.response_headers.on': True,
+    })
+
+
+def _setup_global_config(config=None):
+    if config:  # dictionary or path to config file
+        cherrypy.config.update(config)
+    _set_default_headers()
+
+
+def start_server(host='0.0.0.0',
+                 port=8080,
+                 threads=20,
+                 autoreload=False,
+                 log_to_screen=False,
+                 path_to_access_log=None,
+                 path_to_error_log=None,
+                 **kwargs):
+    conf = {
+        'engine.autoreload_on': autoreload,
+        'log.screen': log_to_screen,
+        'server.socket_host': host,
+        'server.socket_port': port,
+        'server.thread_pool': threads,
+    }
+
+    if path_to_access_log:
+        conf['log.access_file'] = path_to_access_log
+    if path_to_error_log:
+        conf['log.error_file'] = path_to_error_log
+
+    strict_start_server(config=conf, **kwargs)
+
+
+def strict_start_server(config=None, root='/', daemonize=False):
+    _setup_global_config(config)
+
     if daemonize:
         _daemonize()
 
-    conf = {root: {'request.dispatch': _create_dispatcher()}}
-    cherrypy.quickstart(None, root, config=conf)
+    app_config = {
+        root: {'request.dispatch': _create_dispatcher()},
+    }
+
+    cherrypy.quickstart(None, root, config=app_config)
