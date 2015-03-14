bacff0ca6cae1f7488853f565b3030eb49ebad01
Christopher Sira <cbsira@gmail.com>
2015-03-13 21:19:46 -0400

    Revert "confirm query params never overwrite dynamic url components"
    This reverts commit 9aa3a57a289985d24877515995b3f1d589624a8d.
    
    Conflicts:
    	cherryontop/decorators/qp.py

--- cherryontop/decorators/qp.py
+++ cherryontop/decorators/qp.py
@@ -1,8 +1,4 @@
 import functools
-import inspect
-
-import cherrypy
-from cherrypy.lib.httputil import parse_query_string
 
 from cherryontop.errors import InvalidParameter, UnexpectedParameter
 
@@ -11,35 +7,20 @@ def typecast_query_params(*a, **kw):
     allowed, cast_funcs = _get_checks(*a, **kw)
 
     def wrap(f):
-        dynamic_url_args = _positional_arg_names(f)
-
-        if set(allowed) & set(dynamic_url_args):  # disjoint?
-            msg = 'query param and dynamic url component names cannot match'
-            raise ValueError(msg)
-
         @functools.wraps(f)
         def wrapped(*args, **kwargs):
-            query_params = parse_query_string(cherrypy.request.query_string)
-
-            # no param names conflict with dynamic url component names
-            for param in query_params:
-                if param in dynamic_url_args:
-                    raise UnexpectedParameter(param)
-
             # all supplied parameters allowed?
-            for param in query_params:
+            for param in kwargs:
                 if param not in allowed:
                     raise UnexpectedParameter(param)
 
             # typecast params
-            for param, cast in cast_funcs:
-                if param in query_params:
+            for param_name, cast in cast_funcs:
+                if param_name in kwargs:
                     try:
-                        query_params[param] = cast(query_params[param])
+                        kwargs[param_name] = cast(kwargs[param_name])
                     except ValueError:
-                        raise InvalidParameter(param)
-
-            kwargs.update(query_params)
+                        raise InvalidParameter(param_name)
 
             return f(*args, **kwargs)
         return wrapped
@@ -61,10 +42,3 @@ def _get_checks(*args, **kwargs):
         to_cast.append(caster)
 
     return allowed, to_cast
-
-
-def _positional_arg_names(f):
-    spec = inspect.getargspec(f)
-    args = spec.args
-    num_positional = len(args) - len(spec.defaults)
-    return args[:num_positional]
