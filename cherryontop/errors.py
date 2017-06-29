777e7cf575cfc5d4887f18b92e953d4279df8930
Christopher Sira <cbsira@gmail.com>
2017-06-28 20:18:57 -0400

    error http_response_code -> status_code

--- cherryontop/errors.py
+++ cherryontop/errors.py
@@ -5,18 +5,18 @@ import cherrypy
 
 class CherryOnTopError(Exception):
 
-    http_response_code = 500
+    status_code = 500
 
 
 def error_response_handler():
     _, e, _ = cherrypy._cperror._exc_info()
-    status = e.http_response_code if isinstance(e, CherryOnTopError) else 500
+    status = e.status_code if isinstance(e, CherryOnTopError) else 500
 
     cherrypy.response.headers["content-type"] = "application/json"
     cherrypy.response.status = status
     cherrypy.response.body = json.dumps({
         "error": e.__class__.__name__,
-        "http_response_code": status,
+        "status_code": status,
         "message": e.message})
 
     if status == 401:
