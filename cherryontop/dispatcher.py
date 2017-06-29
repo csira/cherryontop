fb1a996b228a47f6fe521c7b897758dbc5fe2c43
Christopher Sira <cbsira@gmail.com>
2017-06-29 09:32:12 -0400

    reqs

--- cherryontop/dispatcher.py
+++ cherryontop/dispatcher.py
@@ -12,7 +12,6 @@ def dispatcher_factory():
     dispatcher = cherrypy.dispatch.RoutesDispatcher()
     for route in routes():
         dispatcher.connect(controller=controller, **route)
-
     return dispatcher
 
 
