e6b5a346ce8792681668d699ffc910f6957fb572
chris <chris.sira@jetty.com>
2017-08-27 18:53:30 -0400

    error handler bug

--- cherryontop/errors.py
+++ cherryontop/errors.py
@@ -10,14 +10,20 @@ class CherryOnTopError(Exception):
 
 def error_response_handler():
     _, e, _ = cherrypy._cperror._exc_info()
-    status = e.status_code if isinstance(e, CherryOnTopError) else 500
+
+    if isinstance(e, CherryOnTopError):
+        status = e.status_code
+        message = e.message
+    else:
+        status = 500
+        message = ""
 
     cherrypy.response.headers["content-type"] = "application/json"
     cherrypy.response.status = status
     cherrypy.response.body = json.dumps({
         "error": e.__class__.__name__,
         "status_code": status,
-        "message": e.message})
+        "message": message})
 
     if status == 401:
         realm = 'xBasic realm="auth required"'
