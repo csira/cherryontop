c3f190a3309591623d9028a8193f7ae186f4504f
chris <chris.sira@jetty.com>
2018-02-16 11:03:07 -0500

    add util imports to root

--- cherryontop/__init__.py
+++ cherryontop/__init__.py
@@ -2,9 +2,10 @@ from cherryontop.decos import get, post, put, delete, route
 from cherryontop.errors import CherryOnTopError
 from cherryontop.requests import deserialize_request_body
 from cherryontop.spinup import start_server
+from cherryontop.utils import get_header, set_header, get_cookie, set_cookie
 
 
 __all__ = [
     "get", "post", "put", "delete", "route",
     "start_server", "CherryOnTopError", "deserialize_request_body",
-]
+    "get_header", "set_header", "get_cookie", "set_cookie"]
